7465aac76edfde62db1aae62e160e44afa4ff9f3
masaya <e.cabbage63@gmail.com>
2015-10-16 15:31:52 +0900

    記事のページにボタン追加

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ for(i=0; i<len; i++){
     console.log('arrayOfUrl['+i+']: '+ arrayOfUrl[i]);
 }
 
-if( arrayOfUrl.indexOf('stock') != -1 ){
-    //ストック一覧ページの場合の処理
-    console.log('stockpage!');
+if( arrayOfUrl.indexOf('stock') == 4 && len == 5 ){
+    console.log('userstockpage!');
+    init_stocklist();
+}else if( arrayOfUrl.indexOf('items') != -1 && len == 6 ){
+    console.log('article page now!');
+    init_article();
 }
 
 
-init_list();
-
-function init_list(){
+function init_stocklist(){
     //ストックした記事毎に解析
     var articleVar = $('article');
     var articleLength = articleVar.length;
@@ -27,7 +28,7 @@ function init_list(){
             function(idx, article){
                 //console.log('[' + idx + ']' + article);
                 //要素追加
-                $(this).find('ul.publicItem_statusList').append(add_button(false,idx));
+                $(this).find('ul.publicItem_statusList').append(add_button_to_stocklist(false,idx));
 
                 //ボタンにイベント追加
                 (function(){
@@ -36,7 +37,7 @@ function init_list(){
                     var txt = "";
                     button.click(function(){
                         button.toggleClass('isRead');
-                        set_button_status(button, button.hasClass('isRead'));
+                        set_button_status_stocklist(button, button.hasClass('isRead'));
                         save_status(articleIds[num], button.hasClass('isRead'));
                     });
                 })();
@@ -45,10 +46,30 @@ function init_list(){
                 articleIds.push($(this).data('uuid'));
             }
     );
-    restore_status(articleIds);
+    restore_status_stocklist(articleIds);
 }
 
-function set_button_status(button, isRead){
+function init_article(){
+    //要素追加
+    $('div.itemsShowHeaderStock').append('<button class="btn btn-default btn-block isReadButton">未読</button>');
+
+    restore_status_article(arrayOfUrl[5]);
+
+    //ボタンにイベント追加
+    (function(){
+        var button = $('div.itemsShowHeaderStock button.isReadButton');
+        console.log(button);
+        button.click(function(){
+            console.log('pushed');
+            button.toggleClass('isRead');
+            set_button_status_article(button, button.hasClass('isRead'));
+            save_status(arrayOfUrl[5], button.hasClass('isRead'));
+        });
+    })();
+
+}
+
+function set_button_status_stocklist(button, isRead){
     var txt;
     var clsOn,clsOff;
     if(isRead){
@@ -63,9 +84,24 @@ function set_button_status(button, isRead){
     button.toggleClass(clsOn,true).toggleClass(clsOff,false).text(txt);
 }
 
+function set_button_status_article(button, isRead){
+    var txt;
+    var clsOn,clsOff;
+    if(isRead){
+        txt = "既読";
+        clsOn = "btn-primary isRead";
+        clsOff = "btn-default";
+    }else{
+        txt = "未読";
+        clsOff = "btn-primary isRead";
+        clsOn = "btn-default";
+    }
+    button.toggleClass(clsOn,true).toggleClass(clsOff,false).text(txt);
+}
+
 
 //未読・既読に応じてタグを挿入
-function add_button(isRead, idx){
+function add_button_to_stocklist(isRead, idx){
     if(isRead){
         return '<li id="itembt' + idx + '" class="publicItem_isRead"><button class="btn btn-success isRead">既読</button></li>'
     }else{
@@ -73,7 +109,7 @@ function add_button(isRead, idx){
     }
 }
 
-
+//ストレージにステータスを保存する
 function save_status(uuid, isRead) {
     var val = {uuid:true}
     val[uuid] = isRead;
@@ -83,7 +119,7 @@ function save_status(uuid, isRead) {
 }
 
 //ボタンに状態を付与する
-function restore_status(idList) {
+function restore_status_stocklist(idList) {
     chrome.storage.local.get(idList,
             function(items) {
                 var itemKeys = Object.keys(items);
@@ -91,15 +127,20 @@ function restore_status(idList) {
                 var button; 
                 for(i=0; i<itemLen; i++){
                     button = $('article[data-uuid = ' + itemKeys[i] + '] button');
-                    set_button_status(button, items[itemKeys[i]]);
-                }
-                if(chrome.extension.lastError !== undefined){
-                    alert('error!');
+                    set_button_status_stocklist(button, items[itemKeys[i]]);
                 }
             });
 }
 
-//デバッグ用
+function restore_status_article(id){
+    chrome.storage.local.get(id,
+            function(items) {
+                    set_button_status_article($('div.itemsShowHeaderStock button.isReadButton'), items[id]);
+            });
+}
+
+
+//デバッグ用にストレージ登録状態を標準出力する
 chrome.storage.onChanged.addListener(function(changes, namespace) {
     for (key in changes) {
         var storageChange = changes[key];
